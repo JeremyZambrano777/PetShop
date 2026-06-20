e2bf8f1d9403128780f7bfc57438d073eac0e9d5
erobles1980 <eroblesp@est.ups.edu.ec>
2026-06-20 17:50:01 -0500

    Avance del home 14h48

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,10 @@ function Home() {
             <h1>Home</h1>
             <h2>Bem Vindo ao PetShop</h2>
             <h2>Escrito por Ernesto Robles</h2>
+            <h2>Data de Publicação: 20/06/2024</h2>
+             <h2>Data de Publicação: 20/06/2024</h2>
+              <h2>Data de Publicação: 20/06/2024</h2>
+               <h2>Data de Publicação: 20/06/2024</h2>
         </>
     )
 }
